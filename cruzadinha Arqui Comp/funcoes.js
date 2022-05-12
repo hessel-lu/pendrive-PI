ffd8ec20097bd5d10721cc82ba2337e6f697daa6
hessel-lu <lucas.hessel@sptech.school>
2022-05-12 19:33:56 -0300

    cruzadinha atualizada 12.05 às 19:33

---
+++ cruzadinha Arqui Comp/funcoes.js
@@ -0,0 +1,63 @@
+    function buttonUm() {
+        alert("Dica:")
+    }
+
+    function buttonDois() {
+        alert("Dica:")
+    }
+
+    function buttonTres() {
+        alert("Dica:")
+    }
+
+    function buttonQuatro() {
+        alert("Dica:")
+    }
+
+    function buttonCinco() {
+        alert("Dica:")
+    }
+
+    function buttonSeis() {
+        alert("Dica:")
+    }
+
+    function buttonSete() {
+        alert("Dica:")
+    }
+
+    function buttonOito() {
+        alert("Dica:")
+    }
+
+    function buttonNove() {
+        alert("Dica:")
+    }
+
+    function buttonDez() {
+        alert("Dica:")
+    }
+
+    function buttonOnze() {
+        alert("Dica")
+    }
+
+    function buttonDoze() {
+        alert("Dica:")
+    }
+
+    function buttonTreze() {
+        alert("Dica:")
+    }
+
+    function buttonQuatorze() {
+        alert("Dica:")
+    }
+
+    function buttonQuinze() {
+        alert("Dica:")
+    }
+
+    function buttonDezesseis() {
+        alert("Dica:")
+    }
