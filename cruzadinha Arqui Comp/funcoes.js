6df1a3a8134532ee53e7fdaafa02b2bb0a08294a
hessel-lu <lucas.hessel@sptech.school>
2022-05-13 01:49:26 -0300

    cruzadinha atualizada 13.05

--- cruzadinha Arqui Comp/funcoes.js
+++ cruzadinha Arqui Comp/funcoes.js
@@ -1,3 +1,4 @@
+    // Funções dos botões/dicas:
     function buttonUm() {
         alert("Dica:")
     }
@@ -60,4 +61,185 @@
 
     function buttonDezesseis() {
         alert("Dica:")
+    }
+    // fim das funções dos botões/dicas.
+
+
+    // Validações das imputs:  
+    function CPU1() {
+        if (c.value == "c") {
+            spanText.innerHTML = 'Boa!'
+        } else {
+            spanText.innerHTML = 'ERRO! tente novamente :,('
+        }
+    } 
+    function CPU2() {
+        if (p.value == "p") {
+            spanText.innerHTML = 'Boa!'
+        } else {
+            spanText.innerHTML = 'ERRO! tente novamente :,('
+        }
+    }
+    function CPU3() {
+        if (u.value == "u") {
+            spanText.innerHTML = 'Boa! você é um maioral ;)'
+        } else {
+            spanText.innerHTML = 'ERRO! tente novamente :,('
+        }
+    }
+
+
+    function ULA2() {
+        if (n.value == "n") {
+            spanText.innerHTML = 'Boa!'
+        } else {
+            spanText.innerHTML = 'ERRO! tente novamente :,('
+        }
+    }
+    function ULA3() {
+        if (i.value == "i") {
+        spanText.innerHTML = 'Boa!'
+    } else {
+        spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+    }
+    function ULA4() {
+        if (d.value == "d") {
+            spanText.innerHTML = 'Boa!'
+        } else {
+            spanText.innerHTML = 'ERRO! tente novamente :,('
+        }
+    }
+    function ULA5() {
+        if (a.value == "a") {
+            spanText.innerHTML = 'Boa!'
+        } else {
+            spanText.innerHTML = 'ERRO! tente novamente :,('
+        }
+    }
+    function ULA6() {
+        if (d1.value == "d") {
+            spanText.innerHTML = 'Boa!'
+        } else {
+            spanText.innerHTML = 'ERRO! tente novamente :,('
+        }
+    }
+    function ULA7() {
+        if (e.value == "e") {
+            spanText.innerHTML = 'Boa!'
+        } else {
+            spanText.innerHTML = 'ERRO! tente novamente :,('
+        }
+    }
+    function ULA8() {
+        if (l.value == "l") {
+            spanText.innerHTML = 'Boa!'
+        } else {
+            spanText.innerHTML = 'ERRO! tente novamente :,('
+        }
+    }
+    function ULA9() {
+        if (o.value == "o") {
+            spanText.innerHTML = 'Boa!'
+        } else {
+            spanText.innerHTML = 'ERRO! tente novamente :,('
+        }
+    }
+    function ULA10() {
+        if (g.value == "g") {
+            spanText.innerHTML = 'Boa!'
+        } else {
+            spanText.innerHTML = 'ERRO! tente novamente :,('
+        }
+    }
+    function ULA11() {
+        if (i1.value == "i") {
+            spanText.innerHTML = 'Boa!'
+        } else {
+            spanText.innerHTML = 'ERRO! tente novamente :,('
+        }
+    }
+    function ULA12() {
+        if (c2.value == "c") {
+            spanText.innerHTML = 'Boa!'
+        } else {
+            spanText.innerHTML = 'ERRO! tente novamente :,('
+        }
+    }
+    function ULA13() {
+        if (a1.value == "a") {
+            spanText.innerHTML = 'Boa!'
+        } else {
+            spanText.innerHTML = 'ERRO! tente novamente :,('
+        }
+    }
+    function ULA14() {
+        if (a3.value == "a") {
+            spanText.innerHTML = 'Boa!'
+        } else {
+            spanText.innerHTML = 'ERRO! tente novamente :,('
+        }
+    }
+    function ULA15() {
+        if (r.value == "r") {
+            spanText.innerHTML = 'Boa!'
+        } else {
+            spanText.innerHTML = 'ERRO! tente novamente :,('
+        }
+    }
+    function ULA16() {
+        if (i2.value == "i") {
+            spanText.innerHTML = 'Boa!'
+        } else {
+            spanText.innerHTML = 'ERRO! tente novamente :,('
+        }
+    }
+    function ULA17() {
+        if (t.value == "t") {
+            spanText.innerHTML = 'Boa!'
+        } else {
+            spanText.innerHTML = 'ERRO! tente novamente :,('
+        }
+    }
+    function ULA18() {
+        if (m.value == "m") {
+            spanText.innerHTML = 'Boa!'
+        } else {
+            spanText.innerHTML = 'ERRO! tente novamente :,('
+        }
+    }
+    function ULA19() {
+        if (e2.value == "e") {
+            spanText.innerHTML = 'Boa!'
+        } else {
+            spanText.innerHTML = 'ERRO! tente novamente :,('
+        }
+    }
+    function ULA20() {
+        if (d2.value == "d") {
+            spanText.innerHTML = 'Boa!'
+        } else {
+            spanText.innerHTML = 'ERRO! tente novamente :,('
+        }
+    }
+    function ULA21() {
+        if (i3.value == "i") {
+            spanText.innerHTML = 'Boa!'
+        } else {
+            spanText.innerHTML = 'ERRO! tente novamente :,('
+        }
+    }
+    function ULA22() {
+        if (c3.value == "c") {
+            spanText.innerHTML = 'Boa!'
+        } else {
+            spanText.innerHTML = 'ERRO! tente novamente :,('
+        }
+    }
+    function ULA23() {
+        if (a4.value == "a") {
+            spanText.innerHTML = 'Boa! você é um maioral ;)'
+        } else {
+            spanText.innerHTML = 'ERRO! tente novamente :,('
+        }
     }
