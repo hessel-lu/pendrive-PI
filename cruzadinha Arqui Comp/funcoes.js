e4bb97064f40628a184c66c58c19232595d367a9
hessel-lu <lucas.hessel@sptech.school>
2022-05-13 21:54:27 -0300

    cruzadinha finalizada ;)

--- cruzadinha Arqui Comp/funcoes.js
+++ cruzadinha Arqui Comp/funcoes.js
@@ -1,66 +1,66 @@
     // Funções dos botões/dicas:
-    function buttonUm() {
-        alert("Dica:")
+    function buttonUm() { 
+        alert("Dica: Parte que representa o cérebro do computador, o componente mais essencial.")
     }
 
     function buttonDois() {
-        alert("Dica:")
+        alert("Dica: Parte responsável por executar somas, subtrações e funções lógicas.")
     }
 
     function buttonTres() {
-        alert("Dica:")
+        alert("Dica: Memória alocada dentro da cpu.")
     }
 
     function buttonQuatro() {
-        alert("Dica:")
+        alert("Dica: Memória que permite ler e gravar, mas perde a informação, sem energia.")
     }
 
     function buttonCinco() {
-        alert("Dica:")
+        alert("Dica: Memória que permite apenas leitura e guarda informação após corte de energia.")
     }
 
     function buttonSeis() {
-        alert("Dica:")
+        alert("Dica: Memória não volátil da Unidade de Comando Eletrônico.")
     }
 
     function buttonSete() {
-        alert("Dica:")
+        alert("Dica: Memória ideal para aparelhos móveis ou com uso de bateria.")
     }
 
     function buttonOito() {
-        alert("Dica:")
+        alert("Dica: Memória não volátil, grava informações e não as perde sem fonte de energia.")
     }
 
     function buttonNove() {
-        alert("Dica:")
+        alert("Dica: Recurso utilizado para poupar o processador na comunicação de periféricos com o computador.")
     }
 
     function buttonDez() {
-        alert("Dica:")
+        alert("Dica: Linha de controle usada para seleção de um circuito integrado conectado a um barramento onde há mais circuitos.")
     }
 
     function buttonOnze() {
-        alert("Dica")
+        alert("Dica: Parte que que transfere o endereço na memória.")
     }
 
     function buttonDoze() {
-        alert("Dica:")
+        alert("Dica: Via elétrica usada para transferir dados.")
     }
 
     function buttonTreze() {
-        alert("Dica:")
+        alert("Dica: Processador com até 4 núcleos, sem Hyper-threading.")
     }
 
     function buttonQuatorze() {
-        alert("Dica:")
+        alert("Dica: Processador com 6 núcleos, com Hyper-Threading.")
     }
 
     function buttonQuinze() {
-        alert("Dica:")
+        alert("Dica: Processador com dois núcleos.")
     }
 
     function buttonDezesseis() {
-        alert("Dica:")
+        alert("Dica: Processador com quatro núcleos ou dois núcleos físicos e dois virtuais.")
     }
     // fim das funções dos botões/dicas.
 
@@ -229,17 +229,271 @@
             spanText.innerHTML = 'ERRO! tente novamente :,('
         }
     }
+    // usando operador ternário
     function ULA22() {
-        if (c3.value == "c") {
-            spanText.innerHTML = 'Boa!'
-        } else {
-            spanText.innerHTML = 'ERRO! tente novamente :,('
-        }
-    }
+        c3.value == "c" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('
+        
+    } 
     function ULA23() {
-        if (a4.value == "a") {
-            spanText.innerHTML = 'Boa! você é um maioral ;)'
-        } else {
-            spanText.innerHTML = 'ERRO! tente novamente :,('
-        }
+        a4.value == "a" ? spanText.innerHTML = 'Boa! você é um maioral ;)' : spanText.innerHTML = 'ERRO! tente novamente :,('
     }
+
+
+    function REG1() {
+        r1.value == "r" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+    function REG2() {
+        e3.value == "e" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+    function REG3() {
+        i4.value == "i" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+    function REG4() {
+        s.value == "s" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+    function REG5() {
+        t1.value == "t" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+    function REG6() {
+        r2.value == "r" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+    function REG7() {
+        a5.value == "a" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+    function REG8() {
+        d3.value == "d" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+    function REG9() {
+        o1.value == "o" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+    function REG10() {
+        r3.value == "r" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+    function REG11() {
+        e4.value == "e" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+    function REG12() {
+        s1.value == "s" ? spanText.innerHTML = 'Boa! você é um maioral ;)' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+
+
+    function RAM1() {
+        a6.value == "a" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,(' 
+    }
+    function RAM2() {
+        m1.value == "m" ? spanText.innerHTML = 'Boa! você é um maioral ;)' : spanText.innerHTML = 'ERRO! tente novamente :,(' 
+    }
+
+
+    function ROM1() {
+        r4.value == "r" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,(' 
+    }
+    function ROM2() {
+        o2.value == "o" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,(' 
+    }
+    function ROM3() {
+        m2.value == "m" ? spanText.innerHTML = 'Boa! você é um maioral ;)' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+
+
+    function EPROM1() {
+        e5.value == "e" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,(' 
+    }
+    function EPROM2() {
+        p1.value == "p" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,(' 
+    }
+    function EPROM3() {
+        r5.value == "r" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,(' 
+    }
+    function EPROM4() {
+        o3.value == "o" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,(' 
+    }
+    function EPROM5() {
+        m3.value == "m" ? spanText.innerHTML = 'Boa! você é um maioral ;)' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+
+
+    function FLASH1() {
+        f.value == "f" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('  
+    }
+    function FLASH2() {
+        l1.value == "l" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,(' 
+    }
+    function FLASH3() {
+        a7.value == "a" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,(' 
+    }
+    function FLASH4() {
+        h.value == "h" ? spanText.innerHTML = 'Boa! você é um maioral ;)' : spanText.innerHTML = 'ERRO! tente novamente :,(' 
+    }
+
+
+    function MEMODEMASSA1() {
+        m4.value == "m" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('   
+    }
+    function MEMODEMASSA2() {
+        m5.value == "m" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('   
+    }
+    function MEMODEMASSA3() {
+        o4.value == "o" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('   
+    }
+    function MEMODEMASSA4() {
+        r6.value == "r" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('   
+    }
+    function MEMODEMASSA5() {
+        i5.value == "i" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('   
+    }
+    function MEMODEMASSA6() {
+        a8.value == "a" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('   
+    }
+    function MEMODEMASSA7() {
+        d4.value == "d" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('   
+    }
+    function MEMODEMASSA8() {
+        m6.value == "m" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('   
+    }
+    function MEMODEMASSA9() {
+        a9.value == "a" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('   
+    }
+    function MEMODEMASSA10() {
+        s2.value == "s" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('   
+    }
+    function MEMODEMASSA11() {
+        s3.value == "s" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('   
+    }
+    function MEMODEMASSA12() {
+        a10.value == "a" ? spanText.innerHTML = 'Boa! você é um maioral ;)' : spanText.innerHTML = 'ERRO! tente novamente :,(' 
+    }
+
+
+    function DMA1() {
+        d5.value == "d" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('    
+    }
+    function DMA2() {
+        a11.value == "a" ? spanText.innerHTML = 'Boa! você é um maioral ;)' : spanText.innerHTML = 'ERRO! tente novamente :,('  
+    }
+
+
+    function CHIPSEL1() {
+        c4.value == "c" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('       
+    }
+    function CHIPSEL2() {
+        h1.value == "h" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+    function CHIPSEL3() {
+        p2.value == "p" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+    function CHIPSEL4() {
+        s4.value == "s" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+    function CHIPSEL5() {
+        e6.value == "e" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+    function CHIPSEL6() {
+        l2.value == "l" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+    function CHIPSEL7() {
+        e7.value == "e" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+    function CHIPSEL8() {
+        c5.value == "c" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+    function CHIPSEL9() {
+        t2.value == "t" ? spanText.innerHTML = 'Boa! você é um maioral ;)' : spanText.innerHTML = 'ERRO! tente novamente :,('    
+    }
+
+
+    function ADRESS1() {
+        d6.value == "d" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+    function ADRESS2() {
+        r7.value == "r" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+    function ADRESS3() {
+        e8.value == "e" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+    function ADRESS4() {
+        s5.value == "s" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+    function ADRESS5() {
+        b.value == "b" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+    function ADRESS6() {
+        u1.value == "u" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+    function ADRESS7() {
+        s6.value == "s" ? spanText.innerHTML = 'Boa! você é um maioral ;)' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+
+
+    function DATABUS1() {
+        d7.value == "d" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+    function DATABUS2() {
+        a12.value == "a" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+    function DATABUS3() {
+        t3.value == "t" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+    function DATABUS4() {
+        b1.value == "b" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+    function DATABUS5() {
+        u2.value == "u" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+    function DATABUS6() {
+        s7.value == "s" ? spanText.innerHTML = 'Boa! você é um maioral ;)' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+
+
+    function I51() {
+        cinco.value == "5" ? spanText.innerHTML = 'Boa! você é um maioral ;)' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+
+
+    function I71() {
+        sete.value == "7" ? spanText.innerHTML = 'Boa! você é um maioral ;)' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+
+    
+    function DUALCORE1() {
+        u3.value == "u" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('  
+    }
+    function DUALCORE2() {
+        a13.value == "a" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('  
+    }
+    function DUALCORE3() {
+        l3.value == "l" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('  
+    }
+    function DUALCORE4() {
+        c6.value == "c" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('  
+    }
+    function DUALCORE5() {
+        o5.value == "o" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('  
+    }
+    function DUALCORE6() {
+        r8.value == "r" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('  
+    }
+    function DUALCORE7() {
+        e9.value == "e" ? spanText.innerHTML = 'Boa! você é um maioral ;)' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+
+    
+    function QUADCORE1() {
+        q.value == "q" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('  
+    }
+    function QUADCORE2() {
+        a14.value == "a" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('  
+    }
+    function QUADCORE3() {
+        d8.value == "d" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('  
+    }
+    function QUADCORE4() {
+        c7.value == "c" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('  
+    }
+    function QUADCORE5() {
+        r9.value == "r" ? spanText.innerHTML = 'Boa!' : spanText.innerHTML = 'ERRO! tente novamente :,('  
+    }
+    function QUADCORE6() {
+        e10.value == "e" ? spanText.innerHTML = 'Boa! você é um maioral ;)' : spanText.innerHTML = 'ERRO! tente novamente :,('
+    }
+    //fim
